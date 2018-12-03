39d217a468ddc38910336026098cdbc97ba8a8d9
Andre Sionek <andresionek91@gmail.com>
2018-12-03 00:39:16 -0200

    update app

--- application.py
+++ application.py
@@ -6,6 +6,7 @@ import json
 application = Flask(__name__)
 
 @application.route('/')
+@application.route('/index')
 def home():
     return render_template('index.html')
 
@@ -13,14 +14,19 @@ def home():
 @application.route('/score', methods=['POST', 'GET'])
 def score():
     if request.method == 'POST':
+        # get result from form and treat it
         result = request.form
-
         treated_input_json = treat_input(result)
+
+        # create header and url
         header = {'Content-Type': 'application/x-www-form-urlencoded'}
         url = 'https://tk9k0fkvyj.execute-api.us-east-2.amazonaws.com/default/top20-predictor'
+
+        # make POST request and load response
         r = requests.post(url, params=treated_input_json, headers=header).json()['body']
         result = json.loads(r)
 
+        # render the html template sending the variables
         return render_template("score.html", score=result['score'], proba=result['proba'])
 
 
